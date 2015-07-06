97ef28e171956d501570eb84546436c044a2e7ec
julianromera <julian.romera@gmail.com>
2015-07-06 20:38:39 +0200

    finished R script that generates code to compare the all of the results

--- scripts/r-compare.sh
+++ scripts/r-compare.sh
@@ -51,9 +51,8 @@ function create_r_file {
 function write_r {
     if [ ! -f "$2" ]; then
         echo "Write error."
-        exit 1    
+        exit 1
     fi
-
     echo_null=`echo "$1" | tee -a "$2"`
     res=$?
     echo_null=`echo "" | tee -a "$2"`
@@ -91,7 +90,7 @@ function read_variable {
         exit 1
     fi
     token="id__${sf}__${jobid}__"
-    var=`echo "$var" | sed 's/ //g' | sed 's/:/ <- /g' | sed 's/^\(.*\)/'$token'\1/'
+    var=`echo "$var" | sed 's/ //g' | sed 's/:/ <- /g' | sed 's/^\(.*\)/'$token'\1/'`
     eval "$4=\"$var\""
 }
 
@@ -103,13 +102,25 @@ function generate_r_variables {
     for jobid in $jobids; do
          verify_file $jobid $sf
     done
- 
+
     for jobid in $jobids; do
          read_variable $jobid $sf "mean_time:" mean_time
          write_r "$mean_time" $results_file
          read_variable $jobid $sf "total_gpus:" gpus
          write_r "$gpus" $results_file
     done
+
+    values=""
+    for jobid in $jobids; do
+        token="id__${sf}__${jobid}__"
+        if [ "x$values" = "x" ]; then
+            values="get(paste('$token','mean_time',sep=''))"
+        else
+            values="$values,get(paste('$token','mean_time',sep=''))"
+        fi
+    done
+    values="c($values)"
+    write_r "vect__${sf} <- $values" $results_file
 
     get_labels "$jobids" $sf labels
     write_r "labels_x <- $labels" $results_file
@@ -118,7 +129,6 @@ function generate_r_variables {
     eval "$4=$n_jobids"
 }
 
-
 function get_labels {
     jobids="$1"
     sf=$2
@@ -126,9 +136,12 @@ function get_labels {
     n_jobids=`echo "$jobids" | wc -w`
     default_labels=""
     for jobid in $jobids; do
-        #read_variable $jobid $sf "Tasks" default_label
-        default_label="paste(get(paste('id__',$jobid,'__total_gpus',sep='')),'-Process',sep='')"
-	default_labels="$default_label $default_labels"
+        if [ "x$default_labels" = "x" ]; then
+            default_labels="paste(get(paste('id__',$sf,'__',$jobid,'__total_gpus',sep='')),'-Process',sep='')"
+        else
+            default_label="paste(get(paste('id__',$sf,'__',$jobid,'__total_gpus',sep='')),'-Process',sep='')"
+            default_labels="$default_labels $default_label"
+        fi
     done
     default_labels=`echo $default_labels | sed 's/ /,/g'`
     default_labels="c($default_labels)"
@@ -141,45 +154,70 @@ function get_labels {
           echo -n "Enter a total $n_jobids label(s) between quoutes. Separate them with spaces: "
           read labels_temp < /dev/tty
           n_labels_temp=`echo "$labels_temp" | wc -w`
-          if [ $n_labels_temp != "$n_jobids"  ]; then
+          if [ $n_labels_temp != "$n_jobids" ]; then
              echo "Error entering label(s). $n_labels_temp label(s) were entered."
-	     echo -n "Re-"	
+         echo -n "Re-"
           else
              continue_loop="n"
              labels_x="c(`echo "$labels_temp" | sed 's/ /,/g'`)"
           fi
        else
           continue_loop="n"
-          labels_x=$default_labels
+          labels_x="$default_labels"
        fi
     done
+
     eval "$3=\"$labels_x\""
 }
 
 function generate_r_plotcode {
     result_file="$1"
     sfs="$2"
-    jobids="$3"
+    n_sfs="$3"
+    jobids="$4"
 
-    matrix=""
-    for sf in $sfs; do 
-       for jobid in $jobids; do
-         
-       do
+    labels_y=`echo "$sfs" | sed 's/ / sf/g' | sed 's/^/sf/'`
+    new_labels_y=""
+    for label_y in $labels_y; do
+        if [ "x$new_labels_y" = "x" ]; then
+            new_labels_y="\"$label_y\""
+        else
+            new_labels_y="$new_labels_y,\"$label_y\""
+        fi
     done
-    matrix="mat <- matrix(labels_y, nrow = length(labels_y), ncol = length(labels_x), byrow = TRUE, \
-        dimnames = list(c($val_list), \
-        labels_x))"
 
+    values=""
+    for sf in $sfs; do
+        token="vect__${sf}"
+        if [ "x$values" = "x" ]; then
+            values="$token"
+        else
+            values="$values,$token"
+        fi
+    done
+    values="c($values)"
+
+    matrix="mat <- matrix($values, nrow = $n_sfs, ncol = length(labels_x), byrow = TRUE, dimnames = list(labels_y, labels_x))"
+    plotcode="par(xpd = TRUE, mar = c(5.1, 4.1, 4.1, 8.1))
+        mp <-  matplot(mat, axes = FALSE,
+        main = '', col = rainbow(length(rownames(mat))), type='c', lty=1, lwd=1)
+        axis(1, at = 1:length(colnames(mat)), labels = labels_x)
+        axis(2, at = seq(0, 1.75, 0.01), labels = seq(0, 1.75, 0.01))
+        legend('topright', inset = c(-0.25, 0), fill = rainbow(length(rownames(mat))),
+        legend = labels_y)
+        title(main = 'Execution on Fermi. Scale factor comparison', font.main = 4)"
+
+    write_r "labels_y <- c($new_labels_y)" $result_file
     write_r "$matrix" $result_file
-    # write_r "line()" $result_file 
+    write_r "$plotcode" $result_file
 }
 
 function build {
     echo ""
     sfs="$1"
     n_sfs=$2
-    token="-`echo "$sfs" | sed 's/ /-/g'`"
+    token=`echo "$sfs" | sed 's/ /\-/g'`
+    token="-$token"
 
     create_r_file "$token" result_file
     old_number_jobids="0"
@@ -188,18 +226,23 @@ function build {
 
         generate_r_variables $sf $result_file jobids new_number_jobids
 
-        if [ $old_number_jobids = "0" ]; then
+        if [ "x$old_number_jobids" = "x0" ]; then
             old_number_jobids=$new_number_jobids
         else
-            if [ "$old_number_jobids" != "$new_number_jobids" ]; then
-                echo "Error. The number of JOBIDs should be the same for each SF.."
+            if [ "x$old_number_jobids" != "x$new_number_jobids" ]; then
+                echo "Error. The number of JOBIDs should be the same for each SF."
                 exit 1
             fi
         fi
-    jobid_list="$jobids $jobid_list"
+
+        if [ "x$jobid_list" = "x" ]; then
+            jobid_list="$jobids"
+        else
+            jobid_list="$jobid_list $jobids"
+        fi
     done
 
-    generate_r_plotcode "$result_file" "$sfs" "$jobid_list"
+    generate_r_plotcode "$result_file" "$sfs" $n_sfs "$jobid_list"
 
     echo "-> Created file \"$result_file\". "
     echo "-> R-Code successfully generated.";
