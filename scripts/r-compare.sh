095614dafd4a4728349c9eac4a2a46735a02cd88
julianromera <julian.romera@gmail.com>
2015-07-06 03:47:59 +0200

    fixed R script execution

--- scripts/r-compare.sh
+++ scripts/r-compare.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 #
 # This script will generate code to display a comparative plot of the mean-time of 1-N JOBIDS in 1-M SCALE_FACTOR
@@ -18,13 +18,20 @@ function filename {
 
 function verify_file {
     jobid=$1
+    sf=$2
     filename $jobid file
     is_valid=`grep "Validation: passed" $file`
+    scale_factor=`tail -125 $file | grep "SCALE"`
+    scale_factor=${scale_factor##*:\ }
 
-    if [ "x$is_valid" != "x" ]; then
-                echo "-> File $file. Validation passed (jobid=$jobid). "
-    else
+    if [ "x$is_valid" = "x" ]; then
         echo "Error. execution ended with error. File $file"
+        exit 1
+    fi
+    if [ "x$scale_factor" = "x$sf" ]; then
+        echo "-> File $file. Validation passed. "
+    else
+        echo "Error. Typed SF does not match file's SF. File $file"
         exit 1
     fi
 }
@@ -53,49 +60,52 @@ function write_r {
 }
 
 function read_jobids {
+    sf=$1
 
-    echo -n "Enter a a list of Slurm's JOBIDs. Separate them with spaces: "
+    echo -n "Enter a list of JOBIDs for SF $sf. Separate them with spaces: "
     read jobids < /dev/tty
     n_jobids=`echo "$jobids" | wc -w`
-    if [ $n_jobids = "0"  ]; then
-       echo "Error entering label(s). 0 label(s) were entered."
-    else
-       jobids="c(`echo "$jobids" | sed 's/ /,/g'`)"
+    if [ $n_jobids -eq 0  ]; then
+       echo "Error entering JOBID(s). 0 JOBID(s) were entered."
+       exit 1
     fi
 
-    eval "$1=$jobids"
-    eval "$2=$n_jobids"
+    eval "$2=\"$jobids\""
+    eval "$3=$n_jobids"
 }
 
 function read_variable {
-    jobid=$1
-    variable=$2
+    jobid="$1"
+    variable="$2"
     filename $jobid file
 
-    var=`grep $variable $file`
+    var=`tail -121 $file | grep $variable | head -1`
     if [ "x$var" = "x" ]; then
         echo "Error. Variable $variable in file $file could not be read."
         exit 1
     fi
     var=`echo "$var" | sed 's/ //g' | sed 's/:/<-/g' | sed 's/^\(.*\)/id__'$jobid'__\1/'`
-    eval "$3=$var"
+    eval "$3=\"$var\""
 }
 
 function iterate_sf {
-    sf=$1
+    sf="$1"
 
-    reads_jobids jobids n_jobids
+    read_jobids $sf jobids n_jobids
+    for jobid in $jobids; do
+         verify_file $jobid $sf
+    done
+ 
     variables=""
-    for jobid in "$jobids" {
-         verify_file jobid
-         read_variable jobid "mean_time" variable
+    for jobid in $jobids; do
+         read_variable $jobid "mean_time:" variable
          variables="$variables $variable"
          get_labels "$jobids" $sf labels
-    }
+    done
     var=`echo "$variables" | sed 's/ /;/g'`
 
-    eval "$2=$labels"
-    eval "$3=$variables"
+    eval "$2=\"$labels\""
+    eval "$3=\"$variables\""
     eval "$4=$new_number_jobids"
 }
 
@@ -106,8 +116,9 @@ function get_labels {
     n_jobids=`echo "$jobids" | wc -w`
     default_labels=""
     for jobid in "$jobids"; do
-        read_variable jobid "Tasks" default_label
-        default_labels="$default_label $default_labels"
+        #read_variable $jobid "Tasks" default_label
+        default_label="a$jobid"
+	default_labels="$default_label $default_labels"
     done
     default_labels=`echo $default_labels | sed 's/ /,/g'`
     default_labels="c($default_labels)"
@@ -119,13 +130,14 @@ function get_labels {
        read yesno < /dev/tty
        if [ "x$yesno" = "xy" ];then
           echo -n "Enter a total $n_jobids label(s) between quoutes. Separate them with spaces: "
-          read labels < /dev/tty
-          n_labels=`echo "$labels" | wc -w`
-          if [ $n_labels != "$n_jobids"  ]; then
-             echo "Error entering label(s). $n_labels label(s) were entered."
+          read labels_temp < /dev/tty
+          n_labels_temp=`echo "$labels_temp" | wc -w`
+          if [ $n_labels_temp != "$n_jobids"  ]; then
+             echo "Error entering label(s). $n_labels_temp label(s) were entered."
+	     echo -n "Re-"	
           else
              continue_loop="n"
-             labels_x="c(`echo "$labels" | sed 's/ /,/g'`)"
+             labels_x="c(`echo "$labels_temp" | sed 's/ /,/g'`)"
           fi
 
        else
@@ -133,7 +145,7 @@ function get_labels {
           labels_x=$default_labels
        fi
     done
-    eval "$3=$labels_x"
+    eval "$3=\"$labels_x\""
 }
 
 
@@ -164,10 +176,10 @@ function build {
 
         iterate_sf $sf labels variables new_number_jobids
 
-        if [ $old_number_jobids -eq 0 ]; then
+        if [ $old_number_jobids = "0" ]; then
             old_number_jobids=new_number_jobids
         else
-            if [ $old_number_jobids -ne $new_number_jobids ]; then
+            if [ "$old_number_jobids" = "$new_number_jobids" ]; then
                 echo "Error. The number of JOBIDs should be the same for each Scale Factor."
                 exit 1
             fi
@@ -186,8 +198,9 @@ function build {
 
 
 if [ $# -lt 1 ] || [ "x$1" = "x-h" ] || [ "x$1" = "x--help" ]; then
-    echo "This script will generate code to display a comparative plot of the mean-time of 1-N JOBIDS in 1-M SCALE_FACTOR"
-    echo "Usage: $0 <SCALE_FACTOR> [SCALE_FACTOR] [SCALE_FACTOR] [SCALE_FACTOR] ..."
+    echo "This script will generate code to display a comparative plot of the mean-times between cale-factors"
+    echo ""
+    echo "Usage: $0 <SCALE_FACTOR> [SCALE_FACTOR] [SCALE_FACTOR] ..."
     exit 1
 fi
 build "$*" $#
