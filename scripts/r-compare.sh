ca614a8eb247a2b15828e5d27e339d52decfd5e0
julianromera <julian.romera@gmail.com>
2015-07-06 13:43:29 +0200

    creating line plots

--- scripts/r-compare.sh
+++ scripts/r-compare.sh
@@ -81,7 +81,8 @@ function read_jobids {
 
 function read_variable {
     jobid="$1"
-    variable="$2"
+    sf=$2
+    variable="$3"
     filename $jobid file
 
     var=`tail -121 $file | grep $variable | head -1`
@@ -89,8 +90,9 @@ function read_variable {
         echo "Error. Variable $variable in file $file could not be read."
         exit 1
     fi
-    var=`echo "$var" | sed 's/ //g' | sed 's/:/ <- /g' | sed 's/^\(.*\)/id__'$jobid'__\1/'`
-    eval "$3=\"$var\""
+    token="id__${sf}__${jobid}__"
+    var=`echo "$var" | sed 's/ //g' | sed 's/:/ <- /g' | sed 's/^\(.*\)/'$token'\1/'
+    eval "$4=\"$var\""
 }
 
 function generate_r_variables {
@@ -103,32 +105,33 @@ function generate_r_variables {
     done
  
     for jobid in $jobids; do
-         read_variable $jobid "mean_time:" mean_time
+         read_variable $jobid $sf "mean_time:" mean_time
          write_r "$mean_time" $results_file
-         read_variable $jobid "total_gpus:" gpus
+         read_variable $jobid $sf "total_gpus:" gpus
          write_r "$gpus" $results_file
     done
 
     get_labels "$jobids" $sf labels
     write_r "labels_x <- $labels" $results_file
 
-    eval "$3=$n_jobids"
+    eval "$3=\"$jobids\""
+    eval "$4=$n_jobids"
 }
 
 
 function get_labels {
     jobids="$1"
     sf=$2
+
     n_jobids=`echo "$jobids" | wc -w`
     default_labels=""
     for jobid in $jobids; do
-        #read_variable $jobid "Tasks" default_label
+        #read_variable $jobid $sf "Tasks" default_label
         default_label="paste(get(paste('id__',$jobid,'__total_gpus',sep='')),'-Process',sep='')"
 	default_labels="$default_label $default_labels"
     done
     default_labels=`echo $default_labels | sed 's/ /,/g'`
     default_labels="c($default_labels)"
-
 
     continue_loop="y"
     while [ "x$continue_loop" = "xy" ]; do
@@ -145,7 +148,6 @@ function get_labels {
              continue_loop="n"
              labels_x="c(`echo "$labels_temp" | sed 's/ /,/g'`)"
           fi
-
        else
           continue_loop="n"
           labels_x=$default_labels
@@ -156,8 +158,21 @@ function get_labels {
 
 function generate_r_plotcode {
     result_file="$1"
+    sfs="$2"
+    jobids="$3"
 
-    write_r "line()" $result_file
+    matrix=""
+    for sf in $sfs; do 
+       for jobid in $jobids; do
+         
+       do
+    done
+    matrix="mat <- matrix(labels_y, nrow = length(labels_y), ncol = length(labels_x), byrow = TRUE, \
+        dimnames = list(c($val_list), \
+        labels_x))"
+
+    write_r "$matrix" $result_file
+    # write_r "line()" $result_file 
 }
 
 function build {
@@ -168,9 +183,10 @@ function build {
 
     create_r_file "$token" result_file
     old_number_jobids="0"
+    jobid_list=""
     for sf in $sfs; do
 
-        generate_r_variables $sf $result_file new_number_jobids
+        generate_r_variables $sf $result_file jobids new_number_jobids
 
         if [ $old_number_jobids = "0" ]; then
             old_number_jobids=$new_number_jobids
@@ -180,10 +196,10 @@ function build {
                 exit 1
             fi
         fi
-
+    jobid_list="$jobids $jobid_list"
     done
 
-    generate_r_plotcode "$result_file"
+    generate_r_plotcode "$result_file" "$sfs" "$jobid_list"
 
     echo "-> Created file \"$result_file\". "
     echo "-> R-Code successfully generated.";
