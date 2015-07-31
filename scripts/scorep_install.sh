eaae2f54a8061dee07d44972342cb581c3912692
julianromera <julian.romera@gmail.com>
2015-07-31 02:43:59 +0200

    changes in order to compile locally openmpi and scorep

--- scripts/scorep_install.sh
+++ scripts/scorep_install.sh
@@ -5,7 +5,7 @@
 
 function exit_error {
   local error=$1
-  if [ $error -neq 0 ]; then
+  if [ $error -ne 0 ]; then
     echo "Error detected. Quitting..."
     exit 1
   fi
@@ -29,8 +29,7 @@ function makedir {
   local dir="$1"
   if [ ! -d $dir ]; then
     mkdir -p $dir
-    res=$?
-    exit_error $res
+    exit_error $?
   fi
 }
 
@@ -41,13 +40,14 @@ function install {
   export OMPI_CC=$cc
   export OMPI_CXX=$cxx
 
-  if [ 1 -eq 2]; then
+  if [ 1 -eq 2 ]; then
+    :
+  fi
   banner "OpenMPI"
   if [ ! -f ${openmpi}.tar.gz ]; then
     section_banner "Downloading"
     wget http://www.open-mpi.org/software/ompi/v1.8/downloads/${openmpi}.tar.gz
-    res=$?
-    exit_error $res
+    exit_error $?
   fi
   if [ ! -d ${openmpi} ]; then
     section_banner "Decompressing"
@@ -55,35 +55,30 @@ function install {
   else
     cd ${openmpi}
     section_banner "Un-Installing"
-    make uninstall 2> /dev/null
+    make -j4 uninstall 2> /dev/null
     section_banner "Cleaning"
-    make clean 2> /dev/null
+    make -j4 clean 2> /dev/null
     cd ..
   fi
   makedir $openmpi_prefix
   cd ${openmpi}
   section_banner "Checking"
   # --with-cuda=$cuda_dir
-  ./configure CC=$cc CXX=$cxx  --prefix=$openmpi_prefix --disable-mpi-f90 --enable-mpirun-prefix-by-default
-  res=$?
-  exit_error $res
+  ./configure CC=$cc CXX=$cxx  --prefix=$openmpi_prefix --enable-mpirun-prefix-by-default
+  exit_error $?
   section_banner "Making"
-  make clean
-  make -j 12
-  res=$?
-  exit_error $res
+  make -j4
+  # exit_error $?
   section_banner "Installing"
-  make install
-  res=$?
-  exit_error $res
+  make -j4 install
+  exit_error $?
   cd ..
 
   banner "Opari"
   if [ ! -f  ${opari}.tar.gz ]; then
     section_banner "Downloading"
     wget http://www.vi-hps.org/upload/packages/opari2/${opari}.tar.gz
-    res=$?
-    exit_error $res
+    exit_error $?
   fi
   if [ ! -d  ${opari} ]; then
     section_banner "Decompressing"
@@ -91,34 +86,29 @@ function install {
   else
     cd ${opari}
     section_banner "Un-Installing"
-    make uninstall 2> /dev/null
+    make -j4 uninstall 2> /dev/null
     section_banner "Cleaning"
-    make clean 2> /dev/null
+    make -j4 clean 2> /dev/null
     cd ..
   fi
   makedir $opari_prefix
   cd ${opari}
   section_banner "Checking"
   ./configure CC=$cc CXX=$cxx  --prefix=$opari_prefix
-  res=$?
-  exit_error $res
+  exit_error $?
   section_banner "Making"
-  make clean
-  make -j 12
-  res=$?
-  exit_error $res
+  make -j4
+  # exit_error $?
   section_banner "Installing"
-  make install
-  res=$?
-  exit_error $res
+  make -j4 install
+  exit_error $?
   cd ..
 
   banner "Cube"
   if [ ! -f  ${cube}.tar.gz ]; then
     section_banner "Downloading"
     wget http://apps.fz-juelich.de/scalasca/releases/cube/4.2/dist/${cube}.tar.gz
-    res=$?
-    exit_error $res
+    exit_error $?
   fi
   if [ ! -d  ${cube} ]; then
     section_banner "Decompressing"
@@ -126,34 +116,29 @@ function install {
   else
     cd ${cube}
     section_banner "Un-Installing"
-    make uninstall 2> /dev/null
+    make -j4 uninstall 2> /dev/null
     section_banner "Cleaning"
-    make clean 2> /dev/null
+    make -j4 clean 2> /dev/null
     cd ..
   fi
   makedir $cube_prefix
   cd ${cube}
   section_banner "Checking"
-  ./configure CC=$cc CXX=$cxx  --prefix=$cube_prefix --without-gui
-  res=$?
-  exit_error $res
+  ./configure --prefix=$cube_prefix --without-gui
+  exit_error $?
   section_banner "Making"
-  make clean
-  make -j 12
-  res=$?
-  exit_error $res
+  make -j4
+  #exit_error $?
   section_banner "Installing"
-  make install
-  res=$?
-  exit_error $res
+  make -j4 install
+  exit_error $?
   cd ..
-  fi
+
   banner "Score-P"
   if [ ! -f  ${scorep}.tar.gz ]; then
     section_banner "Downloading"
   	wget http://www.vi-hps.org/upload/packages/scorep/${scorep}.tar.gz
-    res=$?
-    exit_error $res
+    exit_error $?
   fi
   if [ ! -d  ${scorep} ]; then
     section_banner "Decompressing"
@@ -161,24 +146,23 @@ function install {
   else
     cd ${scorep}
     section_banner "Un-Installing"
-    make uninstall 2> /dev/null
+    make -j4 uninstall 2> /dev/null
     section_banner "Cleaning"
-    make clean 2> /dev/null
+    make -j4 clean 2> /dev/null
     cd ..
   fi
   makedir $scorep_prefix
   cd ${scorep}
   section_banner "Checking"
-  ./configure CC=$cc CXX=$cxx --prefix=$scorep_prefix --with-cube=$cube_prefix --with-opari2=$opari_prefix --with-cuda=$cuda_dir
-  res=$?
-  exit_error $res
+  ./configure  --prefix=$scorep_prefix --with-cube=$cube_prefix \
+                --with-opari2=$opari_prefix  \
+                --with-cuda=$cuda_dir
+  exit_error $?
   section_banner "Making"
-  make clean
-  make -j12
-  res=$?
-  exit_error $res
+  make -j4
+  #exit_error $?
   section_banner "Installing"
-  make install
+  make -j4  install
   return $?
 }
 
@@ -190,7 +174,7 @@ cuda_dir=`echo $nvcc | sed 's,/bin/nvcc$,,'`
 openmpi="openmpi-1.6.5"
 opari="opari2-1.1.2"
 cube="cube-4.2.3"
-scorep="scorep-1.4.2"
+scorep="scorep-1.3"
 
 openmpi_prefix="$HOME/openmpi"
 opari_prefix="$HOME/opari2"
