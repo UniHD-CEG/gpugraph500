c41072b80101b453c6784bc5079412a5ffba506f
julianromera <julian.romera@gmail.com>
2015-10-02 20:03:39 +0200

    automate formatting

---
+++ scripts/astyle.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+astyle=`which astyle`
+
+if [ ! -x $astyle ]; then
+    echo "error:: astyle is not installed"
+    exit 1
+fi
+
+
+$astyle  --style=allman --recursive --options=../cpu_2d/astyle.conf ../cpu_2d/\*.h ../cpu_2d/\*.hpp ../cpu_2d/\*.cuh ../cpu_2d/\*.hh ../cpu_2d/\*.c ../cpu_2d/\*.cpp ../cpu_2d/\*.cu
+
