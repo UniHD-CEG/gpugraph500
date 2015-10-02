57ce270fbf5e146db94fcda1eee7d33eb5818c9f
julianromera <julian.romera@gmail.com>
2015-10-02 21:55:19 +0200

    correct compilation errors

--- cpu_2d/compression/compression.hh
+++ cpu_2d/compression/compression.hh
@@ -13,7 +13,7 @@ public:
     virtual void decompress(T *compressed_fq_64, const int size,
                                /*Out*/ T **uncompressed_fq_64, /*In Out*/size_t &uncompressedsize) =0;
     virtual void verifyCompression(const T *fq, const T *uncompressed_fq_64,
-                                   compressedsizeonst size_t uncompressedsize) =0;
+                                                  const size_t uncompressedsize) =0;
     virtual inline bool isCompressed(const size_t originalsize, const size_t compressedsize) =0;
 };
 
