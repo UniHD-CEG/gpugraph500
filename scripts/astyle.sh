6a6aec54553d5ee1fe9f6c46cf78765e61c2f44e
julianromera <julian.romera@gmail.com>
2015-10-05 20:39:32 +0200

    fix astyle error

--- scripts/astyle.sh
+++ scripts/astyle.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+
 astyle=`which astyle`
 
-if [ ! -x $astyle ]; then
+if [ ! -f "$astyle" ] || [ ! -x "$astyle" ]; then
     echo "error:: astyle is not installed"
     exit 1
+else
+	$astyle  --style=allman --recursive --options=../cpu_2d/astyle.conf ../cpu_2d/\*.h ../cpu_2d/\*.hpp ../cpu_2d/\*.hh ../cpu_2d/\*.c ../cpu_2d/\*.cpp ../cpu_2d/\*.cu
 fi
 
 
-$astyle  --style=allman --recursive --options=../cpu_2d/astyle.conf ../cpu_2d/\*.h ../cpu_2d/\*.hpp ../cpu_2d/\*.hh ../cpu_2d/\*.c ../cpu_2d/\*.cpp ../cpu_2d/\*.cu
 
