055f492049bf15a65eacc6154b65e226ae1af59e
julianromera <julian.romera@gmail.com>
2015-10-20 00:49:35 +0200

    remove unused lines. correct typos. remove old installations when a new version of an app is downloaded

--- scripts/3rdparty_install.sh
+++ scripts/3rdparty_install.sh
@@ -6,6 +6,16 @@
 #
 
 
+# Configuration
+#
+
+temporaldirectory_prefix="$HOME/tmp/"
+installdirectory_prefix="$HOME/"
+
+#
+# End Configuration
+
+
 
 
 #
@@ -123,6 +133,10 @@ function install {
   if [ ! -d ${temporaldirectory_prefix}${shortname} ]; then
     section_banner "Decompressing ${filename} to ${temporaldirectory_prefix}${shortname}"
     if [ ! -d ${temporaldirectory_prefix}${shortname} ]; then
+      # directory to be deleted is inside /home/
+      if [ "x`echo ${temporaldirectory_prefix}${shortname}| grep -i "/home/"`" != "x" ]; then
+        rm -rf ${temporaldirectory_prefix}${shortname} 2> /dev/null
+      fi
       makedir ${temporaldirectory_prefix}${shortname}
     fi
     tar -xzvf ${temporaldirectory_prefix}${filename} -C ${temporaldirectory_prefix}${shortname} --strip-components 1 2> /dev/null
@@ -155,216 +169,8 @@ function install {
 
 
 
-
-
-function old_install {
-  banner "OpenMPI"
-
-  if [ ! -f ${openmpi}.tar.gz ]; then
-    section_banner "Downloading"
-    wget http://www.open-mpi.org/software/ompi/v1.8/downloads/${openmpi}.tar.gz
-    exit_error $?
-  fi
-  if [ ! -d ${temporaldirectory_prefix}${openmpi} ]; then
-    section_banner "Decompressing"
-    tar -xzvf ${openmpi}.tar.gz ${temporaldirectory_prefix}${openmpi}
-  else
-    cd ${openmpi}
-    section_banner "Un-Installing"
-    make -j4 uninstall 2> /dev/null
-    section_banner "Cleaning"
-    make -j4 clean 2> /dev/null
-    cd ..
-  fi
-  makedir $openmpi_installdirectory
-  cd ${openmpi}
-  section_banner "Checking"
-  # --with-cuda=$cuda_dir
-  ./configure CC=$cc CXX=$cxx  --prefix=$openmpi_installdirectory --enable-mpirun-prefix-by-default
-  exit_error $?
-  section_banner "Making"
-  make -j4
-  # exit_error $?
-  section_banner "Installing"
-  make -j4 install
-  exit_error $?
-  cd ..
-
-  banner "Opari"
-  if [ ! -f  ${opari}.tar.gz ]; then
-    section_banner "Downloading"
-    wget http://www.vi-hps.org/upload/packages/opari2/${opari}.tar.gz
-    exit_error $?
-  fi
-  if [ ! -d  ${opari} ]; then
-    section_banner "Decompressing"
-    tar -xzvf ${opari}.tar.gz
-  else
-    cd ${opari}
-    section_banner "Un-Installing"
-    make -j4 uninstall 2> /dev/null
-    section_banner "Cleaning"
-    make -j4 clean 2> /dev/null
-    cd ..
-  fi
-  makedir $opari_installdirectory
-  cd ${opari}
-  section_banner "Checking"
-  ./configure CC=$cc CXX=$cxx  --prefix=$opari_installdirectory
-  exit_error $?
-  section_banner "Making"
-  make -j4
-  # exit_error $?
-  section_banner "Installing"
-  make -j4 install
-  exit_error $?
-  cd ..
-
-  banner "TAU"
-  if [ ! -f  ${tau}.tar.gz ]; then
-    section_banner "Downloading"
-    wget https://www.cs.uoregon.edu/research/tau/tau_releases/${tau}.tar.gz
-    exit_error $?
-  fi
-  if [ ! -d  ${tau} ]; then
-    section_banner "Decompressing"
-    tar -xzvf ${tau}.tar.gz
-  else
-    cd ${tau}
-    section_banner "Un-Installing"
-    make -j4 uninstall 2> /dev/null
-    section_banner "Cleaning"
-    make -j4 clean 2> /dev/null
-    cd ..
-  fi
-  makedir $tau_installdirectory
-  cd $tau
-  section_banner "Checking"
-  ./configure -c++=g++ -cc=gcc -prefix=$tau_installdirectory -cuda=$cuda_dir
-  exit_error $?
-  section_banner "Making & Installing"
-  make -j4 install
-  exit_error $?
-  cd ..
-
-  banner "Cube"
-  if [ ! -f  ${cube}.tar.gz ]; then
-    section_banner "Downloading"
-    wget http://apps.fz-juelich.de/scalasca/releases/cube/4.3/dist/${cube}.tar.gz
-    exit_error $?
-  fi
-  if [ ! -d  ${cube} ]; then
-    section_banner "Decompressing"
-    tar -xzvf ${cube}.tar.gz
-  else
-    cd ${cube}
-    section_banner "Un-Installing"
-    make -j4 uninstall 2> /dev/null
-    section_banner "Cleaning"
-    make -j4 clean 2> /dev/null
-    cd ..
-  fi
-  makedir $cube_installdirectory
-  cd ${cube}
-  section_banner "Checking"
-  ./configure --prefix=$cube_installdirectory #--without-gui
-  exit_error $?
-  section_banner "Making"
-  make -j4
-  #exit_error $?
-  section_banner "Installing"
-  make -j4 install
-  exit_error $?
-  cd ..
-
-
-  banner "Score-P"
-  if [ ! -f  ${scorep}.tar.gz ]; then
-    section_banner "Downloading"
-    wget http://www.vi-hps.org/upload/packages/scorep/${scorep}.tar.gz
-    exit_error $?
-  fi
-  if [ ! -d  ${scorep} ]; then
-    section_banner "Decompressing"
-    tar -xzvf ${scorep}.tar.gz
-  else
-    cd ${scorep}
-    section_banner "Un-Installing"
-    make -j4 uninstall 2> /dev/null
-    section_banner "Cleaning"
-    make -j4 clean 2> /dev/null
-    cd ..
-  fi
-  makedir $scorep_installdirectory
-  cd ${scorep}
-  section_banner "Checking"
-  ./configure   --prefix=$scorep_installdirectory --with-cube=$cube_installdirectory \
-                --with-opari2=$opari_installdirectory  \
-                --with-libcudart=$cuda_dir --enable-mpi --enable-cuda
-  exit_error $?
-  section_banner "Making"
-  make -j4
-  #exit_error $?
-  section_banner "Installing"
-  make -j4  install
-
-
-  banner "Scalasca"
-  if [ ! -f  ${scalasca}.tar.gz ]; then
-    section_banner "Downloading"
-    wget http://apps.fz-juelich.de/scalasca/releases/scalasca/2.2/dist/${scalasca}.tar.gz
-    exit_error $?
-  fi
-  if [ ! -d  ${scalasca} ]; then
-    section_banner "Decompressing"
-    tar -xzvf ${}.tar.gz
-  else
-    cd ${scalasca}
-    section_banner "Un-Installing"
-    make -j4 uninstall 2> /dev/null
-    section_banner "Cleaning"
-    make -j4 clean 2> /dev/null
-    cd ..
-  fi
-  makedir $scalasca_installdirectory
-  cd ${scalasca}
-  section_banner "Checking"
-  ./configure   --prefix=$scalasca_installdirectory --with-cube=${cube_installdirectory}/bin \
-                --with-mpi=openmpi --with-otf2=${scorep_installdirectory}/bin
-  exit_error $?
-  section_banner "Making"
-  make -j4
-  #exit_error $?
-  section_banner "Installing"
-  make -j4  install
-  return $?
-}
-
-
-
-# these versions are compatible.
-# Ubuntu 12. Aug 2015
-# openmpi="openmpi-1.6.5"
-# opari="opari2-1.1.2"
-# cube="cube-4.2.3"
-# scorep="scorep-1.3"
-# tau="tau-2.24"
-# openmpi="openmpi-1.6.5"
-# opari="opari2-1.1.2"
-# cube="cube-4.3.2"
-# scorep="scorep-1.4.1"
-# tau="tau-2.24"
-# scalasca="scalasca-2.2.2"
-# openmpi_installdirectory="$HOME/openmpi"
-# opari_installdirectory="$HOME/opari2"
-# cube_installdirectory="$HOME/cube"
-# scorep_installdirectory="$HOME/scorep"
-# tau_installdirectory="$HOME/tau"
-# scalasca_installdirectory="$HOME/scalasca"
-
-
 #
-# Find binaries. used in creek cluster.
+# Find binaries. used in Creek cluster.
 #
 cxx=`locate bin/g++- | grep "bin/g++-[0-9]" | tail -1`
 cc=`locate bin/gcc- | grep "bin/gcc-[0-9]" | tail -1`
@@ -376,17 +182,6 @@ export PATH=$openmpi_installdirectory/bin:$PATH
 export OMPI_CC=$cc
 export OMPI_CXX=$cxx
 
-
-#
-# Configuration
-#
-
-temporaldirectory_prefix="$HOME/tmp/"
-installdirectory_prefix="$HOME/"
-
-#
-# End Configuration
-#
 
 
 
@@ -403,28 +198,28 @@ array_of_apps[$number_of_apps,3]="CC=$cc CXX=$cxx --enable-mpirun-prefix-by-defa
 
 let number_of_apps++
 array_of_apps[$number_of_apps,1]="Opari" # Name of application
-array_of_apps[$number_of_apps,2]="http://www.vi-hps.org/upload/packages/opari2/${opari}.tar.gz" # Download url
+array_of_apps[$number_of_apps,2]="http://www.vi-hps.org/upload/packages/opari2/opari2-1.1.2.tar.gz" # Download url
 array_of_apps[$number_of_apps,3]="CC=$cc CXX=$cxx" # ./config script's parameters
 get_shortfilename ${array_of_apps[$number_of_apps,2]} shortname
 opari_installdirectory=${installdirectory_prefix}$shortname
 
 let number_of_apps++
 array_of_apps[$number_of_apps,1]="Cube" # Name of application
-array_of_apps[$number_of_apps,2]="http://apps.fz-juelich.de/scalasca/releases/cube/4.3/dist/${cube}.tar.gz" # Download url
+array_of_apps[$number_of_apps,2]="http://apps.fz-juelich.de/scalasca/releases/cube/4.3/dist/cube-4.3.2.tar.gz" # Download url
 array_of_apps[$number_of_apps,3]="--without-gui" # ./config script's parameters
 get_shortfilename ${array_of_apps[$number_of_apps,2]} shortname
 cube_installdirectory=${installdirectory_prefix}$shortname
 
 let number_of_apps++
 array_of_apps[$number_of_apps,1]="Score-P" # Name of application
-array_of_apps[$number_of_apps,2]="http://www.vi-hps.org/upload/packages/scorep/${scorep}.tar.gz" # Download url
+array_of_apps[$number_of_apps,2]="http://www.vi-hps.org/upload/packages/scorep/scorep-1.4.1.tar.gz" # Download url
 array_of_apps[$number_of_apps,3]="--with-cube=$cube_installdirectory --with-opari2=$opari_installdirectory --with-libcudart=$cuda_dir --enable-mpi --enable-cuda" # ./config script's parameters
 get_shortfilename ${array_of_apps[$number_of_apps,2]} shortname
 scorep_installdirectory=${installdirectory_prefix}$shortname
 
 let number_of_apps++
 array_of_apps[$number_of_apps,1]="Scalasca" # Name of application
-array_of_apps[$number_of_apps,2]="http://apps.fz-juelich.de/scalasca/releases/scalasca/2.2/dist/${scalasca}.tar.gz" # Download url
+array_of_apps[$number_of_apps,2]="http://apps.fz-juelich.de/scalasca/releases/scalasca/2.2/dist/scalasca-2.2.2.tar.gz" # Download url
 array_of_apps[$number_of_apps,3]="--with-cube=${cube_installdirectory}/bin --with-mpi=openmpi --with-otf2=${scorep_installdirectory}/bin" # ./config script's parameters
 
 
