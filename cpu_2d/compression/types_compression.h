b924bae5f557f96dddeaa5fc2556e776ded07ac7
julianromera <julian.romera@gmail.com>
2015-12-17 00:23:50 +0100

    fix compression error

--- cpu_2d/compression/types_compression.h
+++ cpu_2d/compression/types_compression.h
@@ -4,12 +4,16 @@
 
 #ifdef _SIMD
 typedef long long compressionType;
+typedef MPI_INT MPI_compressed;
 #elif defined(_SIMD_PLUS)
 typedef unsigned char compressionType;
+typedef MPI_LONG MPI_compressed;
 #elif defined(_SIMT)
 typedef int64_t compressionType;
+typedef MPI_LONG MPI_compressed;
 #else
 typedef int64_t compressionType;
+typedef MPI_LONG MPI_compressed;
 #endif
 
 
